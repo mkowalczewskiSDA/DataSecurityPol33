1b9b47b2c454fe717bde7dbafc841bc2727cc421
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-02-27 12:29:18 +0100

    paginacja

--- src/main/java/com/example/DataSecurity/controller/OrderController.java
+++ src/main/java/com/example/DataSecurity/controller/OrderController.java
@@ -1,11 +1,21 @@
 package com.example.DataSecurity.controller;
 
+import com.example.DataSecurity.model.Order;
 import com.example.DataSecurity.repository.OrderRepository;
 import com.example.DataSecurity.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/order")
@@ -15,8 +25,25 @@ public class OrderController {
     OrderService orderService;
 
     @RequestMapping("/list")
-    public String getOrders(Model model) {
-        model.addAttribute("orders", orderService.getAllOrders());
+    public String getOrders(
+            Model model,
+            @RequestParam("page")Optional<Integer> page) {
+        int currentPage = page.orElse(1);
+        Page<Order> orderPage = orderService.getAllOrdersPaginated(
+                PageRequest.of(currentPage-1, 10));
+        model.addAttribute("orderPage", orderPage);
+
+        int totalPages = orderPage.getTotalPages();
+        if (totalPages >0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            /*List<Integer> pageNumbers = new ArrayList<>();
+            for(int i = 1; i<=totalPages; i++) {
+                pageNumbers.add(i);
+            }*/
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
         return "orders";
     }
 
--- src/main/java/com/example/DataSecurity/repository/OrderRepository.java
+++ src/main/java/com/example/DataSecurity/repository/OrderRepository.java
@@ -1,9 +1,15 @@
 package com.example.DataSecurity.repository;
 
 import com.example.DataSecurity.model.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    Page<Order> findAll(Pageable pageable);
+
 }
+
