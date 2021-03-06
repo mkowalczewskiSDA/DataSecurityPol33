7b2fb399ae5587baf939e3965f208d3b20ccc64d
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-03-06 10:32:35 +0100

    logout

--- src/main/java/com/example/DataSecurity/controller/UserController.java
+++ src/main/java/com/example/DataSecurity/controller/UserController.java
@@ -5,11 +5,17 @@ import com.example.DataSecurity.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -20,6 +26,11 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
 
     @GetMapping("/users")
     public String usersList(Model model,
@@ -49,5 +60,14 @@ public class UserController {
 
     @GetMapping("/login")
     public String login() {return "login";}
+
+    @GetMapping("/logout")
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.isAuthenticated()) {
+            new SecurityContextLogoutHandler().logout(request,response,authentication);
+        }
+        return "redirect:/login?logout";
+    }
 
 }
